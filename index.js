82cb2c2f5f18f9433c4151e9b1483c7fe8485fd1
kai-blt <blaitong@gmail.com>
2020-09-15 07:30:20 -0700

    completed task 3

--- index.js
+++ index.js
@@ -36,15 +36,19 @@ function getFinals(data) {
 };
 console.table(getFinals(fifaData));
 
+
+
+
+
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
-function getYears(/* code here */) {
+function getYears(callback) {
+    let arr = callback(fifaData);
+    let years = arr.map(item => item['Year']);
+    return years;
+}
+console.log('Finals Years: ' + getYears(getFinals));
 
-    /* code here */
-
-};
-
-getYears();
 
 /* Task 4: Implement a higher-order function called `getWinners`, that accepts the callback function `getFinals()` and determine the winner (home or away) of each `finals` game. Return the name of all winning countries in an array called `winners` */ 
 
