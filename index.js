7fa8a7f28d1382ea02e9894524f97cb543026bad
kai-blt <blaitong@gmail.com>
2020-09-14 18:29:35 -0700

    completed task 2

--- index.js
+++ index.js
@@ -30,11 +30,11 @@ console.log('Winner of the 2014 cup final: ' + e);
 
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
-function getFinals(/* code here */) {
-
-    /* code here */
-
+function getFinals(data) {
+    let arr = data.filter(data => data['Stage'] === 'Final');
+    return arr;
 };
+console.table(getFinals(fifaData));
 
 /* Task 3: Implement a higher-order function called `getYears` that accepts the callback function `getFinals`, and returns an array called `years` containing all of the years in the dataset */
 
